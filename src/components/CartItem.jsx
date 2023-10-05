a95729541a8b2930785137da5fbfcdfca0373ae8
Lucas Armaretti <larmaretti@gmail.com>
2023-10-05 19:17:08 -0300

    mejorando monto final

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -5,11 +5,11 @@ const CartItem = ({ item }) => {
   const { deleteItem } = useContext(CartContext)
   return (
     <div className='cartItem'>
-      <img src={item.image} alt={item.name} />
-      <p>{item.name}</p>
-      <p>$USD {item.price}</p>
+      <img src={item.imagen} alt={item.titulo} />
+      <p>{item.titulo}</p>
+      <p>$USD {item.precio}</p>
       <p>Quantity: {item.quantity}</p>
-      <p>Sub total : $USD {item.quantity * item.price}</p>
+      <p>Sub total : $USD {item.quantity * item.precio}</p>
       <Button variant='solid' colorScheme='red' size='sm' onClick={() => deleteItem(item.id)}>X</Button>
     </div>
   )
